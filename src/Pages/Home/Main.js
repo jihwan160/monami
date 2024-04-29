ca6891a4a6354e4ae5045be09c48070e3b9c2b39
jihwan160 <dark160@naver.com>
2024-04-29 16:24:30 +0900

    Product 추가

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import './Main.scss'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const VisualImg = styled.div`
     width: 100%;
@@ -92,6 +98,61 @@ const NewCoverBot = styled.div`
     gap: 22px;
 `;
 
+const ProductCover = styled.div`
+    width: 100%;
+    background: #eee;
+    text-align: center;
+    padding-top: 70px;
+
+    & > h2{
+        font-size: 32px;
+        font-weight: 600;
+        letter-spacing: 2px;
+        color: #000;
+        line-height: 120%;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    & > h2::after {
+        content: "";
+        display: block;
+        width: 26px;
+        height: 4px;
+        margin: 10px 0;
+        background: #c40f39;
+        position: relative;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    & > div {
+        margin: 40px 0;
+    }
+
+    & > div > li {
+        display: inline-block;
+        width: 153px;
+        height: 32px;
+        text-align: center;
+        line-height: 32px;
+        color: #898989;
+        border: 2px solid #bcbcbc;
+        cursor: pointer;
+        margin-left: 4px;
+    }
+    & > div > li:nth-child(1) {
+        margin-left: 0;
+    }
+`;
+
+const SlideInfo = styled.div`
+    
+    height: 102px;
+    padding: 0 30px;
+    background: #fff;
+    text-align: left;
+`;
+
 const Main = () => {
 
     const Visual = () => {
@@ -146,12 +207,101 @@ const Main = () => {
         )
     }
 
+    const Product = () => {
+
+        const list = [
+            {
+                img :  `${process.env.PUBLIC_URL}img/premium1.jpg`,
+                cate : '프리미엄펜',
+                name : '153 리스펙트 리파인',
+            },
+            {
+                img : `${process.env.PUBLIC_URL}img/premium2.jpg`,
+                cate : '만년필',
+                name : '153 아이디 만년필',
+            },
+        ]
+
+
+        switch('premium') {
+            case 'premium' :
+                return(
+                    <ProductCover>
+                        <h2>BEST PRODUCT</h2>
+                        <p>언제 어디서나 생활 속에 함께하는 모나미</p>
+                        <div>
+                            <li className='on'>프리미엄 펜</li>
+                            <li>펜·펜슬</li>
+                            <li>마카·컬러링</li>
+                            <li>노트·사무용품</li>
+                        </div>
+                        <div>
+                            <Swiper
+                                spaceBetween={10}
+                                slidesPerView={7}
+                                pagination={{ clickable: true }}
+                            >
+                                {list.map((i,index) => (
+                                <SwiperSlide className='slide' key={index}>
+                                    <div className='imgCover'>
+                                        <img src={i.img} alt='index' />
+                                    </div>
+                                    <SlideInfo>
+                                        <p className='cate'>{i.cate}</p>
+                                        <h2 className='name'>{i.name}</h2>
+                                    </SlideInfo>
+                                </SwiperSlide>
+                                ))}
+
+                            </Swiper>
+                        </div>
+                    </ProductCover>
+                )
+
+            case 'pencil' :
+                return( 
+                    <ProductCover>
+                        <h2>BEST PRODUCT</h2>
+                        <p>언제 어디서나 생활 속에 함께하는 모나미</p>
+                        <div>
+                            <li className='on'>프리미엄 펜</li>
+                            <li>펜·펜슬</li>
+                            <li>마카·컬러링</li>
+                            <li>노트·사무용품</li>
+                        </div>
+                        <div>
+                            <Swiper
+                                spaceBetween={10}
+                                slidesPerView={7}
+                                pagination={{ clickable: true }}
+                            >
+                                {list.map((i,index) => (
+                                <SwiperSlide className='slide' key={index}>
+                                    <div className='imgCover'>
+                                        <img src={i.img} alt='index' />
+                                    </div>
+                                    <SlideInfo>
+                                        <p className='cate'>{i.cate}</p>
+                                        <h2 className='name'>{i.name}</h2>
+                                    </SlideInfo>
+                                </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </div>
+                    </ProductCover>
+                )
+            default : 
+                <div></div>
+        }
+
+    }
 
 
     return (
         <div>
             <Visual />
             <NewArea />
+            <Product />
         </div>
     )
 }
