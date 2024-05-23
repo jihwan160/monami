5eb3964219d5ef31f90d9d747f94a5d599bb7b76
jihwan160 <dark160@naver.com>
2024-05-23 10:50:24 +0900

    company 추가

--- src/Pages/Introduce/Introduce.js
+++ src/Pages/Introduce/Introduce.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Home/Header'
 import Footer from '../Home/Footer'
 import styled from 'styled-components'
@@ -67,6 +67,7 @@ const IntroduceTab = styled.div`
     }
 `;
 
+// 탭 타이틀
 const IntroduceTitle = styled.div`
     font-size: 38px;
     font-weight: 600;
@@ -86,7 +87,7 @@ const IntroduceTitle = styled.div`
         margin: 5px auto 95px;
     }
 `;
-
+// CEO
 const IntroduceWrap = styled.div`
     max-width: 1400px;
     margin: 0 auto;
@@ -170,11 +171,174 @@ const IntroduceWrap = styled.div`
 
 `;
 
+// COMPANY
+
+const IntroduceConpany = styled.div`
+    max-width: 1400px;
+    margin: 0 auto;
+    & .num_title {
+        font-size: 23px;
+        color: #000;
+        font-weight: 300;
+        margin-bottom: 30px;
+        & .num {
+            display: inline-block;
+            text-align: center;
+            vertical-align: top;
+            margin-top: 5px;
+            margin-right: 5px;
+            width: 29px;
+            height: 29px;
+            line-height: 29px;
+            background: #c40f39;
+            font-family: 'Montserrat', sans-serif;
+            font-size: 17px;
+            color: #fff;
+        }
+    }
+    & .subsidiary {
+        
+        &::after {
+            content: '';
+            display: block;
+            clear: both;
+        }
+        & .sub1 {
+            float: left;
+            margin-bottom: 18px;
+        }
+        & .sub2 {
+            float: right;
+            margin-bottom: 18px;
+        }
+        & .sub3 {
+            float: left;
+        }
+        & .sub4 {
+            float: right;
+        }
+    }
+    
+`;
+
 const Introduce = () => {
 
     const CeoArea = () => {
 
-        const Ceo = () => {
+        const [activeTab2, setActiveTab2] = useState('company');
+
+        const handleTabClick2 = (tab) => {
+            setActiveTab2(tab);
+        }
+
+        const renderContent = () => {
+            switch(activeTab2) {
+                case 'ceo' :
+                    return(
+                        <>
+                            <IntroduceTitle>
+                                CEO <span>MESSAGE</span>
+                            </IntroduceTitle>
+                            <IntroduceWrap>
+                                <div className='pht_area'>
+                                    <div className='pht'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/message_ceo.jpg`} alt='message_ceo' />
+                                        <div className='bgbox'></div>
+                                    </div>
+                                    <div className='slg'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/message_slg_bg.png`} alt='message_slg_bg' />
+                                        <div className='textarea'>
+                                            항상 당신 곁의<br />
+                                            <span>소중한 친구</span>로 남겠습니다.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='ceo_message'>
+                                    <div className='ceol'>
+                                        <p>
+                                            안녕하십니까? (주)모나미 대표이사 송하경입니다.<br />
+                                            언제나 한결같이 모나미에 애정과 관심을 보여주시는 고객 및 주주 여러분들께 깊은 감사를 드립니다.
+                                        </p>
+                                        <p>
+                                            1960년 창립된 광신화학공업사에서 출발한 모나미의 역사가 어느덧 반백년이 넘었습니다.<br />
+                                            지금의 모나미를 있게한 ‘모나미 153’볼펜은 지난 50여 년간 꾸준한 사랑을 받으며<br />
+                                            모나미를 필기구의 대명사이자 국민 브랜드로 성장시켰습니다.<br />
+                                            이처럼 많은 분들의 사랑에 힘입어 문구 업계의 혁신을 주도할 수 있었으며,<br />
+                                            사인펜, 프러스펜, 네임펜, 보드 마카 등 수 많은 히트작들을 선보일 수 있었습니다.
+                                        </p>
+                                        <p>
+                                            필기구를 넘어 종합문구 분야의 선도적인 기업으로 건실한 성장을 이뤄온 모나미는 국내시장 뿐 아니라<br />
+                                            해외시장에서도 꾸준한 성장을 거둬 전세계 다양한 국가로 수출되고 있습니다.
+                                        </p>
+                                        <p>
+                                            하지만 모나미는 지난 50여 년간의 성과에 만족하지 않고 새로운 출발에 나서려고 합니다.
+                                        </p>
+                                    </div>
+                                    <div className='ceor'>
+                                        <p>
+                                            종합문구분야의 선두 자리를 넘어 문구와 유통을 아우르는 새로운 비즈니스 모델을 만들어 나가고 있습니다.<br />
+                                            하루가 다르게 변하는 트렌드와 시대의 변화에 대처하기 위해서<br />
+                                            모나미의 기업 문화 역시 새롭게 만들어 나가고 있습니다.<br />
+                                            미래를 긍정적으로 바라보면서 준비하고, 항상 즐거운 변화를 추구하는 기업이 되겠습니다.<br />
+                                            시장에 대해서는 항상 겸손하고, 고객에 대해서는 항상 함께하는 친구 같은 마음과 자세로 대할 것입니다.
+                                        </p>
+                                        <p>
+                                            앞으로 모나미는<br />
+                                            <span className='black'>'오래된 친구지만 항상 신선하고 놀라움으로 가득찬, 그리고 미래를 위해 쉼 없이 노력하는'</span>기업이 되고자<br />
+                                            합니다. 모나미의 새로운 변화와 도전을 지켜봐 주시길 바라며,<br />
+                                            앞으로도 변함없는 사랑과 격려를 부탁드립니다.			
+                                        </p>
+                                        <p>
+                                            감사합니다.
+                                        </p>
+                                        <p className='sign'>
+                                            (주)모나미 대표 
+                                            <img src={`${process.env.PUBLIC_URL}img/introduce/message_sign.jpg`} alt='' />
+                                        </p>
+                                    </div>
+                                </div>
+                            </IntroduceWrap>
+                        </>
+                    )
+                case 'company' :
+                    return(
+                        <>
+                            <IntroduceTitle>
+                                COMPANY <span>INFO</span>
+                            </IntroduceTitle>
+                            <IntroduceConpany>
+                                <h2 className='num_title'>
+                                    <span className='num'>01</span>
+                                    계열사 소개
+                                </h2>
+                                <div className='subsidiary'>
+                                    <div className='sub1'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/company/company_info2_img1.jpg`} alt='company_info2_img1' />
+                                        <div></div>
+                                    </div>
+                                    <div className='sub2'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/company/company_info2_img2.jpg`} alt='company_info2_img2' />
+                                        <div></div>
+                                    </div>
+                                    <div className='sub3'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/company/company_info2_img3.jpg`} alt='company_info2_img3' />
+                                        <div></div>
+                                    </div>
+                                    <div className='sub4'>
+                                        <img src={`${process.env.PUBLIC_URL}img/introduce/company/company_info2_img4.jpg`} alt='company_info2_img4' />
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </IntroduceConpany>
+                        </>
+                    )
+                default :
+                    return(<></>)
+            }
+        }
+
+
+        const MonamiIntroduce = () => {
             return(
                 <IntroduceContainer>
                     <IntroduceTop>
@@ -186,10 +350,10 @@ const Introduce = () => {
                     </IntroduceTop>
                     <IntroduceTab>
                             <ul>
-                                <li className='zmfflr'>
+                                <li onClick={()=>{handleTabClick2('ceo')}} className={activeTab2 === 'ceo' ? 'zmfflr' : ''}>
                                     CEO 메세지
                                 </li>
-                                <li>
+                                <li onClick={()=>{handleTabClick2('company')}} className={activeTab2 === 'company' ? 'zmfflr' : ''}>
                                     회사정보
                                 </li>
                                 <li>
@@ -215,76 +379,16 @@ const Introduce = () => {
                                 </li>
                             </ul>
                     </IntroduceTab>
-                    <IntroduceTitle>
-                        CEO <span>MESSAGE</span>
-                    </IntroduceTitle>
-                    <IntroduceWrap>
-                        <div className='pht_area'>
-                            <div className='pht'>
-                                <img src={`${process.env.PUBLIC_URL}img/introduce/message_ceo.jpg`} alt='message_ceo' />
-                                <div className='bgbox'></div>
-                            </div>
-                            <div className='slg'>
-                                <img src={`${process.env.PUBLIC_URL}img/introduce/message_slg_bg.png`} alt='message_slg_bg' />
-                                <div className='textarea'>
-                                    항상 당신 곁의<br />
-                                    <span>소중한 친구</span>로 남겠습니다.
-                                </div>
-                            </div>
-                        </div>
-                        <div className='ceo_message'>
-                            <div className='ceol'>
-                                <p>
-                                    안녕하십니까? (주)모나미 대표이사 송하경입니다.<br />
-                                    언제나 한결같이 모나미에 애정과 관심을 보여주시는 고객 및 주주 여러분들께 깊은 감사를 드립니다.
-                                </p>
-                                <p>
-                                    1960년 창립된 광신화학공업사에서 출발한 모나미의 역사가 어느덧 반백년이 넘었습니다.<br />
-                                    지금의 모나미를 있게한 ‘모나미 153’볼펜은 지난 50여 년간 꾸준한 사랑을 받으며<br />
-                                    모나미를 필기구의 대명사이자 국민 브랜드로 성장시켰습니다.<br />
-                                    이처럼 많은 분들의 사랑에 힘입어 문구 업계의 혁신을 주도할 수 있었으며,<br />
-                                    사인펜, 프러스펜, 네임펜, 보드 마카 등 수 많은 히트작들을 선보일 수 있었습니다.
-                                </p>
-                                <p>
-                                    필기구를 넘어 종합문구 분야의 선도적인 기업으로 건실한 성장을 이뤄온 모나미는 국내시장 뿐 아니라<br />
-                                    해외시장에서도 꾸준한 성장을 거둬 전세계 다양한 국가로 수출되고 있습니다.
-                                </p>
-                                <p>
-                                    하지만 모나미는 지난 50여 년간의 성과에 만족하지 않고 새로운 출발에 나서려고 합니다.
-                                </p>
-                            </div>
-                            <div className='ceor'>
-                                <p>
-                                    종합문구분야의 선두 자리를 넘어 문구와 유통을 아우르는 새로운 비즈니스 모델을 만들어 나가고 있습니다.<br />
-                                    하루가 다르게 변하는 트렌드와 시대의 변화에 대처하기 위해서<br />
-                                    모나미의 기업 문화 역시 새롭게 만들어 나가고 있습니다.<br />
-                                    미래를 긍정적으로 바라보면서 준비하고, 항상 즐거운 변화를 추구하는 기업이 되겠습니다.<br />
-                                    시장에 대해서는 항상 겸손하고, 고객에 대해서는 항상 함께하는 친구 같은 마음과 자세로 대할 것입니다.
-                                </p>
-                                <p>
-                                    앞으로 모나미는<br />
-                                    <span className='black'>'오래된 친구지만 항상 신선하고 놀라움으로 가득찬, 그리고 미래를 위해 쉼 없이 노력하는'</span>기업이 되고자<br />
-                                    합니다. 모나미의 새로운 변화와 도전을 지켜봐 주시길 바라며,<br />
-                                    앞으로도 변함없는 사랑과 격려를 부탁드립니다.			
-                                </p>
-                                <p>
-                                    감사합니다.
-                                </p>
-                                <p className='sign'>
-                                    (주)모나미 대표 
-                                    <img src={`${process.env.PUBLIC_URL}img/introduce/message_sign.jpg`} alt='' />
-                                </p>
-                            </div>
-                        </div>
-                    </IntroduceWrap>
+                    {renderContent()}
                 </IntroduceContainer>
             )
         }
+        
 
         return(
             <>
                 <Header />
-                <Ceo />
+                <MonamiIntroduce />
                 <Footer />
             </>
         )
